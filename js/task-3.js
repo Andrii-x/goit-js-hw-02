56460e84fa76c7953e3cd7dadf0ada746e77b7d7
Andrii_lishchynskyi <lav95g@gmail.com>
2026-05-20 21:59:48 -0400

    update

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,7 @@
 function checkForSpam(message) {
   const lowerCaseMessage = message.toLowerCase();
   return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
-
-
+}
 
 console.log(checkForSpam("Latest technology news")); // false
 console.log(checkForSpam("JavaScript weekly newsletter")); // false
@@ -11,4 +10,3 @@ console.log(checkForSpam("Amazing SalE, only tonight!")); // true
 console.log(checkForSpam("Trust me, this is not a spam message")); // true
 console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
 console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
-}
